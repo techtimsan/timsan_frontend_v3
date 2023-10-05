3422cf077e5e1db7e2935faa97dd004990ac007a
Ridwan Adefemi Odubona <omaralli8088@gmail.com>
2023-10-05 10:08:32 +0000

    google map showing - needs activation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@nextui-org/button"
 import news from "../data/news.json"
 import { NewsCard } from "@/components/cards"
 import { Link } from "@nextui-org/link"
+import { ChapterMap } from "@/components/utils"
 
 export default function Home() {
   return (
@@ -24,7 +25,14 @@ export default function Home() {
       <section className="custom__section">
         <h3>TILETS</h3>
         <h4 className="text-green font-semibold text-xl">Our Session</h4>
+
+        
       </section>
+      
+      {/* google map */}
+      <div>
+        <ChapterMap />
+      </div>
       <Footer />
     </main>
   )
--- src/components/utils/ChapterMap.tsx
+++ src/components/utils/ChapterMap.tsx
@@ -1,26 +1,60 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
+"use client"
+
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api"
+import { useMemo } from "react"
+import { CircularProgress } from "@nextui-org/progress"
+
+const mapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string
 
 const ChapterMap = () => {
+  const libraries = useMemo(() => ["places"], [])
+
   const mapContainerStyle = {
     height: "400px",
     width: "100%",
   }
 
-  const center = {
-    lat: 37.7749, // Replace with your desired latitude
-    lng: -122.4194, // Replace with your desired longitude
+  const mapCenter = useMemo(
+    () => ({
+      lat: 37.7749, // Replace with your desired latitude
+      lng: -122.4194, // Replace with your desired longitude
+    }),
+    []
+  )
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  )
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: mapsApiKey,
+    libraries: libraries as any,
+  })
+
+  if (!isLoaded) {
+    return (
+      <div>
+        <CircularProgress label="Loading Google Map..." color="secondary" />
+      </div>
+    )
   }
+
   return (
-    <LoadScript googleMapsApiKey="">
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={10}
-      >
-        {/* Add markers or other components here */}
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <GoogleMap
+      options={mapOptions}
+      mapContainerStyle={mapContainerStyle}
+      center={mapCenter}
+      zoom={14}
+      mapTypeId={google.maps.MapTypeId.ROADMAP}
+    >
+      {/* Add markers or other components here */}
+      <Marker position={mapCenter} />
+    </GoogleMap>
   )
 }
 
